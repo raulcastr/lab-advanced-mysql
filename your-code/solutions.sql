e4879d637d489d970392b99a1bbdf7a10dae3807
raulcastr <raulcastmartit@gmail.com>
2020-11-11 19:30:45 +0100

    adding solutions

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -5,7 +5,7 @@ USE publications;
 -- STEP 1 & 2
 SELECT ta.title_id , ta.au_id , 
 	   ROUND(t.advance * ta.royaltyper / 100) AS advance , 
-       ROUND(SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) )AS total_royalty
+       SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS total_royalty
 FROM titleauthor AS ta
 
 INNER JOIN titles AS t
@@ -19,7 +19,7 @@ ORDER BY total_royalty DESC;
 
 -- STEP 3
 
-SELECT au_id, advance+total_royalty AS profits
+SELECT au_id, SUM(advance+total_royalty) AS profits
 FROM (SELECT ta.title_id , ta.au_id , 
 	   ROUND(t.advance * ta.royaltyper / 100) AS advance , 
        ROUND(SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) )AS total_royalty
@@ -31,7 +31,8 @@ ON ta.title_id = s.title_id
 GROUP BY ta.au_id,ta.title_id
 ORDER BY total_royalty DESC) ro_adv
 GROUP BY au_id
-ORDER BY profits DESC;
+ORDER BY profits DESC
+LIMIT 3;
 
 
 -- CHALLENGE 2
@@ -55,15 +56,16 @@ ORDER BY total_royalty DESC;
 
 -- query with temporary table:
 
-SELECT au_id, advance+total_royalty AS profits
+SELECT au_id, SUM(advance+total_royalty) AS profits
 FROM ro_adv
 GROUP BY au_id
-ORDER BY profits DESC;
+ORDER BY profits DESC
+LIMIT 3;
 
 -- CHALLENGE 3 
-
+DROP TABLE IF EXISTS author_profits;
 CREATE TABLE author_profits AS
-(SELECT au_id, advance+total_royalty AS profits
+(SELECT au_id, SUM(advance+total_royalty) AS profits
 FROM ro_adv
 GROUP BY au_id
 ORDER BY profits DESC);
